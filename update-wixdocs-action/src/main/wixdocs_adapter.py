0044599207df27fe5c4e08f89b274250525cbe06
Moriya Tzubara <moriyat@wix.com>
2022-01-11 15:32:05 +0200

    Update wixdocs_adapter.py

--- update-wixdocs-action/src/main/wixdocs_adapter.py
+++ update-wixdocs-action/src/main/wixdocs_adapter.py
@@ -1,11 +1,12 @@
 import requests
 
 def update_wixdocs(path, owner, repo, wixdocs_api):
+    full_path = f"{repo}/{path}"
     headers = {"Content-Type": "application/json"}
-    data = {"org": owner, "url": f"{repo}/{path}"}
+    data = {"org": owner, "url": full_path}
     
     response = requests.post(wixdocs_api, data=data, headers=headers)
-    print(f"wixdos api response for {owner}/{path}: status - {response} content - {response.content}")
+    print(f"wixdos api response for {owner}/{full_path}: status - {response} content - {response.content}")
 
 def update_wixdocs_with(paths, owner, repo, wixdocs_api):
     for path in paths:
